21457016acd6ebe4f853afa5fc734b6ebe29327d
jorlar <jqern.larsen@gmail.com>
2022-07-11 14:51:13 +0200

    :sparkles: Done invaders JS

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+// Create game-grid and shooter
 const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
+let currentShooterIndex = 202
+let width = 15;
+let direction = 1;
+let invadersID = 0;
+let goingRight = true;
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement('div')
@@ -15,6 +22,7 @@ const alienInvaders = [
     30,31,32,33,34,35,36,37,38,39
 ]
 
+// Invaders logic
 function draw() {
     for (let i =0; i < alienInvaders.length; i++) {
         squares[alienInvaders[i]].classList.add('invader')
@@ -22,3 +30,71 @@ function draw() {
 }
 
 draw();
+
+function remove() {
+    for (let i =0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
+
+squares[currentShooterIndex].classList.add('shooter')
+
+// Moving shooter 
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+            break
+        case 'ArrowRight':
+            if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+document.addEventListener('keydown', moveShooter)
+
+// Moving invaders
+function moveInvaders() {
+    // Determine invaders position in grid
+    const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length -1] % width === width -1
+    remove()
+
+    if (rightEdge && goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width +1
+            direction = -1
+            goingRight = false
+        }
+    }
+
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width -1
+            direction = 1
+            goingRight = true
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction
+    }
+
+    draw()
+
+    if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+        resultsDisplay.innerHTML = 'Game Over'
+        clearInterval(invadersID)
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        if(alienInvaders[i] > (squares.length + width)) {
+            resultsDisplay.innerHTML = 'Game Over'
+            clearInterval(invadersID)
+        }
+    }
+}
+
+// Move invaders around
+invadersID = setInterval(moveInvaders, 500)
